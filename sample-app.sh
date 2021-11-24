23b7a7e558e1c8cc0752161d150ad31c680d4433
aratechlabs <86350162+aratechlabs@users.noreply.github.com>
2021-11-25 10:55:45 +1300

    Added file to allow the build to work!

---
+++ sample-app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp sample_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t sampleapp .
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker ps -a 
