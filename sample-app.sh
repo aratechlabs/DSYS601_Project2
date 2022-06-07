7e2460b351fb51c609bf26aea25c7c8af0d9c85b
aratechlabs <86350162+aratechlabs@users.noreply.github.com>
2022-06-08 10:33:07 +1200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -13,6 +13,7 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY  network_equipment.json /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
