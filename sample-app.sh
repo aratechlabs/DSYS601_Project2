321b0b5a902eec0c61cf555c2060a9dde93e7c24
aratechlabs <86350162+aratechlabs@users.noreply.github.com>
2022-06-08 17:58:44 +1200

    Update and rename sample_app.sh to sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -2,15 +2,15 @@
 
 mkdir tempdir
 
-cp -f sample_app.py tempdir/.
+cp -f assessment_code.py tempdir/.
 cp -f network_equipment.json tempdir/.
 
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY  assessment_code.py /home/myapp/" >> tempdir/Dockerfile
 echo "COPY  network_equipment.json /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/assessment_code.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
