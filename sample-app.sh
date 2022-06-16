b47de54ad71181c76e22a3acd18e515630083a70
aratechlabs <86350162+aratechlabs@users.noreply.github.com>
2022-06-16 13:54:54 +1200

    Placed Python Code app in background
    Made Python Code app run in background via an & symbol added on the end of the command

--- sample-app.sh
+++ sample-app.sh
@@ -10,7 +10,7 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  assessment_code.py /home/myapp/" >> tempdir/Dockerfile
 echo "COPY  network_equipment.json /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/assessment_code.py" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/assessment_code.py &" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
